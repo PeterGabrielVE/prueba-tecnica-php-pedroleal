e4cb17f45b624030ab7cea80a4d147cd421cfde0
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-25 13:58:52 -0400

    test update

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,10 +3,14 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use App\Http\Controllers\UserController;
+use App\Repositories\UserRepository;
+use App\Factories\UserFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\User;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Http\JsonResponse;
 class UserTest extends TestCase
 {
     /**
@@ -15,6 +19,21 @@ class UserTest extends TestCase
      * @return void
      */
     use RefreshDatabase;
+
+    public function testIndex()
+    {
+        $userRepository = new UserRepository();
+        $this->app->instance(UserRepository::class, $userRepository);
+
+        $userFactory = new UserFactory();
+        $this->app->instance(UserFactory::class, $userFactory);
+
+        $controller = new UserController($userRepository,$userFactory);
+        $response = $controller->index();
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertArrayHasKey('data', json_decode($response->getContent(), true));
+    }
 
     public function test_create_user()
     {
@@ -27,16 +46,32 @@ class UserTest extends TestCase
             'password' => '123456788',
         ];
 
-        //$response = $this->post(route('api/user/store'), $data);
-
-        //$response->assertStatus(302)->assertRedirect(route('allUsers'));
-        
         $this->json('POST', 'api/user/store',$data)
             ->assertStatus(200);
-           /* ->assertJson([
-                'code' => '200',
-                'message' => 'Hotel Review updated.',
-            ]);
-        //$this->assertDatabaseHas('api/allUsers', $data);*/
+        
+    }
+
+    public function testUpdateUser()
+    {
+        /*$user = factory(User::class)->create();
+        $newData = [
+            'name' => 'New Name',
+            'email' => 'newemail@example.com',
+            // Include other fields to update
+        ];
+
+        $response = $this->json('PUT', "/users/{$user->id}", $newData, ['Accept' => 'application/json']);
+
+        $response->assertStatus(200)
+                ->assertJson([
+                    'data' => true // Assuming the update method returns a boolean
+                ]);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'name' => 'New Name',
+            'email' => 'newemail@example.com',
+            // Include other fields to update
+        ]);*/
     }
 }
